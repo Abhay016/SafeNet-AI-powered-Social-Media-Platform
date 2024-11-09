601fe9f95abf454f391c59ecb33317a425842dd2
Abhay <jaiswal.abhay161@gmail.com>
2024-11-09 21:06:31 +0530

    InitialCommit

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,6 @@ import postRoute from "./routes/post.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./socket/socket.js";
 import path from "path";
-// const fetch = require('node-fetch');
- 
 dotenv.config();
 
 
